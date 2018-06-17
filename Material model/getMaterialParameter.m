b3da704d659f806b1f9f18352c0a580e676cd463
Benjamin <bennyschnabel@me.com>
2018-06-17 09:57:39 +0200

    Create getMaterialParameter.m

---
+++ Material model/getMaterialParameter.m
@@ -0,0 +1,8 @@
+% Function that provides the material parameters
+
+function parameter = getMaterialParameter()
+    %  Material-designation
+    parameter.materialDesignation = ('PA 2200');
+    % Layer thickness [m]
+    parameter.layerThickness = 0.1 * 10^-4;
+end
